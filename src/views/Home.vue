b78cfba5659e683b455a53aea56a7aa34170c5e9
liangwei0101 <liangwei0101@outlook.com>
2018-08-28 22:18:51 +0800

    增加主页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,39 +19,215 @@
       </mu-carousel>
     </div>
 
-    <mu-list style="max-height: 100%;" textline="three-line">
-      <mu-sub-header>今天</mu-sub-header>
-
+    <mu-list textline="two-line">
+      <mu-sub-header>新书上架</mu-sub-header>
       <mu-divider/>
-
-      <mu-list-item-action v-for="(book,index) in booDatakList" :key="index">
-        <mu-list-item avatar="" :ripple="false" button>
-          <mu-list-item-action>
-            <mu-avatar>
-              <img src="http://101.132.124.171:8000/book.jpg">
-            </mu-avatar>
-          </mu-list-item-action>
-          <mu-list-item-content>
-            <mu-list-item-title>{{book.name}}</mu-list-item-title>
-            <mu-list-item-sub-title>
-              <span style="color: rgba(0, 0, 0, .87)">{{book.author}}</span>
-              <br> {{book.introduce}}
-            </mu-list-item-sub-title>
-          </mu-list-item-content>
-        </mu-list-item>
-        <mu-divider/>
-      </mu-list-item-action>
-      
+      <mu-list-item avatar :ripple="false" button @click="goToBookDetail">
+        <mu-list-item-action>
+          <mu-avatar>
+            <img src="http://101.132.124.171:8000/bookInfo.jpg">
+          </mu-avatar>
+        </mu-list-item-action>
+        <mu-list-item-content>
+          <mu-list-item-title>java数据库系统开发实例导航</mu-list-item-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">作者：王小明</mu-list-item-sub-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">
+            架上剩余：2本
+          </mu-list-item-sub-title>
+        </mu-list-item-content>
+        <mu-list-item-action>
+          <mu-list-item-after-text>8.0分</mu-list-item-after-text>
+          <div>
+            <mu-icon size='12' left value="favorite_border"></mu-icon>
+            12
+            <mu-icon size='12' left value="mode_comment"></mu-icon>
+            14
+          </div>
+        </mu-list-item-action>
+      </mu-list-item>
+      <mu-divider></mu-divider>
+      <mu-list-item avatar :ripple="false" button>
+        <mu-list-item-action>
+          <mu-avatar>
+            <img src="http://101.132.124.171:8000/bookInfo.jpg">
+          </mu-avatar>
+        </mu-list-item-action>
+        <mu-list-item-content>
+          <mu-list-item-title>java数据库系统开发实例导航</mu-list-item-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">作者：王小明</mu-list-item-sub-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">
+            架上剩余：2本
+          </mu-list-item-sub-title>
+        </mu-list-item-content>
+        <mu-list-item-action>
+          <mu-list-item-after-text>8.0分</mu-list-item-after-text>
+          <div>
+            <mu-icon size='12' left value="favorite_border"></mu-icon>
+            12
+            <mu-icon size='12' left value="mode_comment"></mu-icon>
+            14
+          </div>
+        </mu-list-item-action>
+      </mu-list-item>
+      <mu-divider></mu-divider>
+      <mu-list-item avatar :ripple="false" button>
+        <mu-list-item-action>
+          <mu-avatar>
+            <img src="http://101.132.124.171:8000/bookInfo.jpg">
+          </mu-avatar>
+        </mu-list-item-action>
+        <mu-list-item-content>
+          <mu-list-item-title>java数据库系统开发实例导航</mu-list-item-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">作者：王小明</mu-list-item-sub-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">
+            架上剩余：2本
+          </mu-list-item-sub-title>
+        </mu-list-item-content>
+        <mu-list-item-action>
+          <mu-list-item-after-text>8.0分</mu-list-item-after-text>
+          <div>
+            <mu-icon size='12' left value="favorite_border"></mu-icon>
+            12
+            <mu-icon size='12' left value="mode_comment"></mu-icon>
+            14
+          </div>
+        </mu-list-item-action>
+      </mu-list-item>
+      <mu-divider></mu-divider>
     </mu-list>
 
-    <mu-container class="bottom">
+    <mu-list textline="two-line">
+      <mu-sub-header>全部图书</mu-sub-header>
+      <mu-divider/>
+      <mu-list-item avatar :ripple="false" button>
+        <mu-list-item-action>
+          <mu-avatar>
+            <img src="http://101.132.124.171:8000/bookInfo.jpg">
+          </mu-avatar>
+        </mu-list-item-action>
+        <mu-list-item-content>
+          <mu-list-item-title>java数据库系统开发实例导航</mu-list-item-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">作者：王小明</mu-list-item-sub-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">
+            架上剩余：2本
+          </mu-list-item-sub-title>
+        </mu-list-item-content>
+        <mu-list-item-action>
+          <mu-list-item-after-text>8.0分</mu-list-item-after-text>
+          <div>
+            <mu-icon size='12' left value="favorite_border"></mu-icon>
+            12
+            <mu-icon size='12' left value="mode_comment"></mu-icon>
+            14
+          </div>
+        </mu-list-item-action>
+      </mu-list-item>
+      <mu-divider></mu-divider>
+      <mu-list-item avatar :ripple="false" button>
+        <mu-list-item-action>
+          <mu-avatar>
+            <img src="http://101.132.124.171:8000/bookInfo.jpg">
+          </mu-avatar>
+        </mu-list-item-action>
+        <mu-list-item-content>
+          <mu-list-item-title>java数据库系统开发实例导航</mu-list-item-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">作者：王小明</mu-list-item-sub-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">
+            架上剩余：2本
+          </mu-list-item-sub-title>
+        </mu-list-item-content>
+        <mu-list-item-action>
+          <mu-list-item-after-text>8.0分</mu-list-item-after-text>
+          <div>
+            <mu-icon size='12' left value="favorite_border"></mu-icon>
+            12
+            <mu-icon size='12' left value="mode_comment"></mu-icon>
+            14
+          </div>
+        </mu-list-item-action>
+      </mu-list-item>
+      <mu-divider></mu-divider>
+      <mu-list-item avatar :ripple="false" button>
+        <mu-list-item-action>
+          <mu-avatar>
+            <img src="http://101.132.124.171:8000/bookInfo.jpg">
+          </mu-avatar>
+        </mu-list-item-action>
+        <mu-list-item-content>
+          <mu-list-item-title>java数据库系统开发实例导航</mu-list-item-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">作者：王小明</mu-list-item-sub-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">
+            架上剩余：2本
+          </mu-list-item-sub-title>
+        </mu-list-item-content>
+        <mu-list-item-action>
+          <mu-list-item-after-text>8.0分</mu-list-item-after-text>
+          <div>
+            <mu-icon size='12' left value="favorite_border"></mu-icon>
+            12
+            <mu-icon size='12' left value="mode_comment"></mu-icon>
+            14
+          </div>
+        </mu-list-item-action>
+      </mu-list-item>
+      <mu-divider></mu-divider>
+      <mu-list-item avatar :ripple="false" button>
+        <mu-list-item-action>
+          <mu-avatar>
+            <img src="http://101.132.124.171:8000/bookInfo.jpg">
+          </mu-avatar>
+        </mu-list-item-action>
+        <mu-list-item-content>
+          <mu-list-item-title>java数据库系统开发实例导航</mu-list-item-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">作者：王小明</mu-list-item-sub-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">
+            架上剩余：2本
+          </mu-list-item-sub-title>
+        </mu-list-item-content>
+        <mu-list-item-action>
+          <mu-list-item-after-text>8.0分</mu-list-item-after-text>
+          <div>
+            <mu-icon size='12' left value="favorite_border"></mu-icon>
+            12
+            <mu-icon size='12' left value="mode_comment"></mu-icon>
+            14
+          </div>
+        </mu-list-item-action>
+      </mu-list-item>
+      <mu-divider></mu-divider>
+      <mu-list-item avatar :ripple="false" button>
+        <mu-list-item-action>
+          <mu-avatar>
+            <img src="http://101.132.124.171:8000/bookInfo.jpg">
+          </mu-avatar>
+        </mu-list-item-action>
+        <mu-list-item-content>
+          <mu-list-item-title>java数据库系统开发实例导航</mu-list-item-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">作者：王小明</mu-list-item-sub-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">
+            架上剩余：2本
+          </mu-list-item-sub-title>
+        </mu-list-item-content>
+        <mu-list-item-action>
+          <mu-list-item-after-text>8.0分</mu-list-item-after-text>
+          <div>
+            <mu-icon size='12' left value="favorite_border"></mu-icon>
+            12
+            <mu-icon size='12' left value="mode_comment"></mu-icon>
+            14
+          </div>
+        </mu-list-item-action>
+      </mu-list-item>
+    </mu-list>
+
+    <!-- <mu-container class="bottom">
       <mu-bottom-nav :value.sync="shift" shift>
         <mu-bottom-nav-item value="index" title="首页" icon="home"></mu-bottom-nav-item>
         <mu-bottom-nav-item value="music" title="商城 " icon="local_grocery_store"></mu-bottom-nav-item>
         <mu-bottom-nav-item value="books" title="联系人" icon="account_box"></mu-bottom-nav-item>
         <mu-bottom-nav-item value="pictures" title="我的" icon="perm_identity"></mu-bottom-nav-item>
       </mu-bottom-nav>
-    </mu-container>
+    </mu-container> -->
 
   </mu-paper>
 </template>
@@ -60,55 +236,60 @@
 import { reqBookList } from "@/api/book";
 
 export default {
-  data() {
-    return {
-      booDatakList: [],
-      images: [
-        "http://101.132.124.171:8000/1.jpg",
-        "http://101.132.124.171:8000/2.jpg",
-        "http://101.132.124.171:8000/3.jpg"
-      ],
-      shift: "index"
-    };
-  },
-  mounted() {
-    reqBookList()
-      .then(response => {
-        this.booDatakList = response.data;
-        console.log(this.booDatakList);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+    data() {
+        return {
+            booDatakList: [],
+            images: [
+                "http://101.132.124.171:8000/1.jpg",
+                "http://101.132.124.171:8000/2.jpg",
+                "http://101.132.124.171:8000/3.jpg"
+            ],
+            shift: "index"
+        };
+    },
+    mounted() {
+        reqBookList()
+            .then(response => {
+                this.booDatakList = response.data;
+                console.log(this.booDatakList);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    },
+    methods: {
+      goToBookDetail () {
+        this.$router.push({ name: 'HomeDetail', params: { userId: '111' }})
+      } 
+    }
 };
 </script>
 
 <style>
 .swipper {
-  margin-left: 1px;
-  margin-right: 1px;
-  margin-top: 48px;
+    margin-left: 1px;
+    margin-right: 1px;
+    margin-top: 48px;
 }
 
 .demo-list-wrap {
-  height: 100%;
-  margin-bottom: 45px;
+    height: 100%;
+    margin-bottom: 45px;
 }
 .top {
-  position: fixed;
-  z-index: 40;
-  width: 100%;
-  max-width: 100%;
-  top: 0px;
+    position: fixed;
+    z-index: 40;
+    width: 100%;
+    max-width: 100%;
+    top: 0px;
 }
 .bottom {
-  position: fixed;
-  z-index: 40;
-  width: 100%;
-  max-width: 100%;
-  background-color: #2196f3;
-  bottom: 0px;
+    position: fixed;
+    z-index: 40;
+    width: 100%;
+    max-width: 100%;
+    background-color: #2196f3;
+    bottom: 0px;
 }
 .mu-list {
 }
